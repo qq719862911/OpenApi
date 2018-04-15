8d23246704997c72bb58de1c99cbb00c50f02006
guohaiyi <516877626@qq.com>
2018-04-15 20:09:53 +0800

    完成userGroup的sdk

--- UserCenter/UC.NETSDK.Tests/Program.cs
+++ UserCenter/UC.NETSDK.Tests/Program.cs
@@ -12,8 +12,45 @@ namespace UC.NETSDK.Tests
         static void Main(string[] args)
         {
             UserApi user = new UserApi("qqqqq", "12345", "http://127.0.0.1:8888/api/v1/");
-            var u = user.GetByIdAsync(1).Result;
-            Console.WriteLine(u.Id+" "+u.NickName+" "+u.PhoneNum+" ");
+
+
+            //var u = user.GetByIdAsync(1).Result;
+            //Console.WriteLine(u.Id+" "+u.NickName+" "+u.PhoneNum+" ");
+
+            //var id = user.AddNewAsync("13872303747", "Aun", "qqqqq").Result;
+            // Console.WriteLine(id+" ");
+
+            //var res = user.CheckLoginAsync("13272303747", "qqqqq").Result;
+            // Console.WriteLine(res + " ");
+
+            //var u = user.GetByPhoneNumAsync("13272303747").Result;
+            //Console.WriteLine(u.Id + " " + u.NickName + " " + u.PhoneNum + " ");
+
+
+            UserGroupApi userGroup = new UserGroupApi("qqqqq", "12345", "http://127.0.0.1:8888/api/v1/");
+            // userGroup.AddUserToGroupAsync(2, 3).Wait();
+
+            //var userGroupArr = userGroup.GetGroupsAsync(3).Result;
+            //foreach (var ug in userGroupArr)
+            //{
+            //    Console.WriteLine(ug.Id+" Name:"+ug.Name);
+            //}
+
+            var ug1 = userGroup.GetByIdAsync(2).Result;
+            Console.WriteLine("Group信息："+ug1.Id + " UserGroupName:" + ug1.Name );
+
+            Console.WriteLine("该Group中的User：");
+
+            var userArr = userGroup.GetGroupUsersAsync(2).Result;
+            foreach (var u in userArr)
+            {
+                Console.WriteLine(u.Id + " Name:" + u.NickName+"  "+u.PhoneNum);
+            }
+
+            userGroup.RemoveUserFromGroupAsync(2, 3).Wait();
+            Console.WriteLine("删除成功");
+
+            Console.WriteLine("ok");
             Console.ReadKey();
         }
     }
--- UserCenter/UserCenter.NETSDK/UserGroupApi.cs
+++ UserCenter/UserCenter.NETSDK/UserGroupApi.cs
@@ -7,118 +7,111 @@ using System.Threading.Tasks;
 
 namespace UserCenter.NETSDK
 {
-    public class UserApi
+   public class UserGroupApi
     {
         private string appKey;
         private string appSecret;
         private string serverRoot;
-        public UserApi(string appKey, string appSecret, string serverRoot)
+        public UserGroupApi(string appKey, string appSecret, string serverRoot)
         {
             this.appKey = appKey;
             this.appSecret = appSecret;
             this.serverRoot = serverRoot;
         }
 
-        public async Task<long> AddNewAsync(string phoneNum, string nickName, string password)
-        {
-            SDKClient client =new SDKClient(appKey, appSecret, serverRoot);
-            Dictionary<string, object> data = new Dictionary<string, object>();
-            data["phoneNum"] = phoneNum;
-            data["nickName"] = nickName;
-            data["password"] = password;
-            var result = await client.GetAsync("User/AddNew", data);
-            if (result.StatusCode == System.Net.HttpStatusCode.OK)
-            {
-                //因为返回的报文体是新增id：{5}
-                //使用newtonsoft把json格式反序列化为long
-                return JsonConvert.DeserializeObject<long>(result.Result);
-            }
-            else
-            {
-                throw new ApplicationException("AddNew失败，状态码"
-                    + result.StatusCode + "，响应报文" + result.Result);
-            }
-        }
-
-        public async Task<bool> UserExistsAsync(string phoneNum)
-        {
-            SDKClient client = new SDKClient(appKey, appSecret, serverRoot);
-            Dictionary<string, object> data = new Dictionary<string, object>();
-            data["phoneNum"] = phoneNum;
-            var result = await client.GetAsync("User/UserExists", data);
-            if (result.StatusCode == System.Net.HttpStatusCode.OK)
-            {
-                //因为返回的报文体是新增id：{5}
-                //使用newtonsoft把json格式反序列化为long
-                return JsonConvert.DeserializeObject<bool>(result.Result);
-            }
-            else
-            {
-                throw new ApplicationException("UserExists失败，状态码"
-                    + result.StatusCode + "，响应报文" + result.Result);
-            }
-        }
-
-        public async Task<bool> CheckLoginAsync(string phoneNum, string password)
-        {
-            SDKClient client = new SDKClient(appKey, appSecret, serverRoot);
-            Dictionary<string, object> data = new Dictionary<string, object>();
-            data["phoneNum"] = phoneNum;
-            data["password"] = password;
-            var result = await client.GetAsync("User/CheckLogin", data);
-            if (result.StatusCode == System.Net.HttpStatusCode.OK)
-            {
-                //因为返回的报文体是新增id：{5}
-                //使用newtonsoft把json格式反序列化为long
-                return JsonConvert.DeserializeObject<bool>(result.Result);
-            }
-            else
-            {
-                throw new ApplicationException("CheckLogin 失败，状态码"
-                    + result.StatusCode + "，响应报文" + result.Result);
-            }
-        }
-
-        
-
-        public async Task<User> GetByIdAsync(long id)
+        public async Task<UserGroup> GetByIdAsync(long id)
         {
             SDKClient client = new SDKClient(appKey, appSecret, serverRoot);
             Dictionary<string, object> data = new Dictionary<string, object>();
             data["id"] = id;
-            var result = await client.GetAsync("User/GetById", data);
+            var result = await client.GetAsync("UserGroup/GetById", data);
             if (result.StatusCode == System.Net.HttpStatusCode.OK)
             {
                 //因为返回的报文体是新增id：{5}
                 //使用newtonsoft把json格式反序列化为long
-                return JsonConvert.DeserializeObject<User>(result.Result);
+                return JsonConvert.DeserializeObject<UserGroup>(result.Result);
             }
             else
             {
-                throw new ApplicationException("GetById失败，状态码"
+                throw new ApplicationException("GetById 失败，状态码"
                     + result.StatusCode + "，响应报文" + result.Result);
             }
         }
 
-        public async Task<User> GetByPhoneNumAsync(string phoneNum)
+        public async Task<UserGroup[]> GetGroupsAsync(long userId)
         {
             SDKClient client = new SDKClient(appKey, appSecret, serverRoot);
             Dictionary<string, object> data = new Dictionary<string, object>();
-            data["phoneNum"] = phoneNum;
-            var result = await client.GetAsync("User/GetByPhoneNum", data);
+            data["userId"] = userId;
+            var result = await client.GetAsync("UserGroup/GetGroups", data);
             if (result.StatusCode == System.Net.HttpStatusCode.OK)
             {
                 //因为返回的报文体是新增id：{5}
                 //使用newtonsoft把json格式反序列化为long
-                return JsonConvert.DeserializeObject<User>(result.Result);
+                return JsonConvert.DeserializeObject<UserGroup[]>(result.Result);
             }
             else
             {
-                throw new ApplicationException("GetByPhoneNum 失败，状态码"
+                throw new ApplicationException("GetGroups 失败，状态码"
                     + result.StatusCode + "，响应报文" + result.Result);
             }
         }
 
+        public async Task<User[]> GetGroupUsersAsync(long userGroupId)
+        {
+            SDKClient client = new SDKClient(appKey, appSecret, serverRoot);
+            Dictionary<string, object> data = new Dictionary<string, object>();
+            data["userGroupId"] = userGroupId;
+            var result = await client.GetAsync("UserGroup/GetGroupUsers", data);
+            if (result.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                //因为返回的报文体是新增id：{5}
+                //使用newtonsoft把json格式反序列化为long
+                return JsonConvert.DeserializeObject<User[]>(result.Result);
+            }
+            else
+            {
+                throw new ApplicationException("GetGroupUsers 失败，状态码"
+                    + result.StatusCode + "，响应报文" + result.Result);
+            }
+        }
+        public async Task AddUserToGroupAsync(long userGroupId, long userId)
+        {
+            SDKClient client = new SDKClient(appKey, appSecret, serverRoot);
+            Dictionary<string, object> data = new Dictionary<string, object>();
+            data["userGroupId"] = userGroupId;
+            data["userId"] = userId;
+            var result = await client.GetAsync("UserGroup/AddUserToGroup", data);
+            if (result.StatusCode == System.Net.HttpStatusCode.NoContent)
+            {
+                //因为返回的报文体是新增id：{5}
+                //使用newtonsoft把json格式反序列化为long
+            }
+            else
+            {
+                throw new ApplicationException("AddUserToGroup 失败，状态码"
+                    + result.StatusCode + "，响应报文" + result.Result);
+            }
+        }
+        
+        public async Task RemoveUserFromGroupAsync(long userGroupId, long userId)
+        {
+            SDKClient client = new SDKClient(appKey, appSecret, serverRoot);
+            Dictionary<string, object> data = new Dictionary<string, object>();
+            data["userGroupId"] = userGroupId;
+            data["userId"] = userId;
+            var result = await client.GetAsync("UserGroup/RemoveUserFromGroup", data);
+            if (result.StatusCode == System.Net.HttpStatusCode.NoContent)
+            {
+                //因为返回的报文体是新增id：{5}
+                //使用newtonsoft把json格式反序列化为long
+            }
+            else
+            {
+                throw new ApplicationException("RemoveUserFromGroup 失败，状态码"
+                    + result.StatusCode + "，响应报文" + result.Result);
+            }
+        }
 
     }
 }
